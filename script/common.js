e118283fd1d6869747ca5dd19d4206aa803c52ed
seomyeong <89440710+seomyeong@users.noreply.github.com>
2021-10-10 20:55:47 +0900

    Add files via upload

--- script/common.js
+++ script/common.js
@@ -1,18 +1,20 @@
 /*--------------------슬라이드----------------------- */
 $('.snb').hide()
+
 /* 주요네비-전체내리기 */
 // $('#gnbTitle').hover(function(){
 //     $('.snb').stop().slideDown(500)
 // }, function(){
 //     $('.snb').stop().slideUp(500)
 // })
-/* 주요네비 개별 내리기 */
+
+/* 주요네비-개별 내리기 */
 $('#gnbTitle>li').on('mouseenter',function(){
     $('.snb').not( $(this).children('.snb') ).hide()
-    $(this).children('.snb').slideDown(300)
+    $(this).children('.snb').stop().slideDown(300)
 })
 $('#gnbTitle>li').on('mouseleave',function(){
-    $('.snb').hide()
+    $('.snb').stop().slideUp(300)
 })
 
 /* nav - 스크롤 한번 내리면 fix시키기 */
@@ -22,7 +24,6 @@ $('#gnbTitle>li').on('mouseleave',function(){
 
 let state=1
 document.addEventListener('wheel',function(e){
-    // console.log(e.wheelDelta)
     if(e.wheelDelta<0 && state==1){
         state=0
         $('#navFix').css({position:'fixed',boxShadow:'0 0 10px rgba(0, 0, 0, 0.3)'})
@@ -32,13 +33,42 @@ document.addEventListener('wheel',function(e){
     }
 })
 /* 메인배너 슬라이드 */
-$('.sliderBtn div').on('click',function(){
+var num=0; //변수 num은 slideOn클래스 이동을 위한 변수
+let liWidth=$('.sliderList').width()
+let sliderLeft= $('.sliderList').css('left')
+
+function sliding(){
+    num++
+    if( $('.sliderList').css('left') == "-200%"){
+        num=0
+        $('.sliderList').animate({left:0})
+    }else{
+        $('.sliderList').animate({left:"-=100%"})
+    }
+    $('.sliderBtn div').removeClass('slideOn')
+    $('.sliderBtn div:eq('+num+')').addClass('slideOn')
+    console.log('sliderLeft = '+$('.sliderList').css('left'))
+
+}
+// let timer1=setInterval(sliding,2000)
+
+$('.sliderBtn div').on('click',function(e){
+    // clearInterval(timer1)
+    // timer1=setInterval(sliding,2000)
+
     let pos=$(this).index()*(-100)+'%'
-    $('.sliderList').animate({left:pos})
+    $('.sliderList:not(:animated)').stop().animate({left:pos})
 
     $('.sliderBtn div').removeClass('slideOn')
     $(this).addClass('slideOn')
+
+    e.preventDefault()
 })
+
+
+
+
+
 
 
 
@@ -50,7 +80,7 @@ $('#tasteLeft').on('click',function(){ //마지막이미지가 첫번째로 prep
 })
 $('#tasteRight').on('click',function(){ //첫번째이미지가 맨뒤로 append. ul의 marginLeft이동
     $('#list ul:not(:animated)').animate({marginLeft:-imgwidth}, 500, function(){
-        $('#list ul').append( $('#list ul li:first') ).css({marginLeft:0})
+        $('#list ul').append( $('#list ul li:first') ).css({marginLeft:'20px'})
     })
 })
 
@@ -58,6 +88,8 @@ $('#tasteRight').on('click',function(){ //첫번째이미지가 맨뒤로 append
 
 
 /* ------------------best-------------------- */
+// $('#steadyList,#thisweekList').hide()
+
 
 /* $('#bestWrap>section>div>ul>li>a') 클릭시 bestProduct에 이미지 뜨게하는 클릭이벤트 */
 $('#bestWrap>section>div>ul>li>a').on('click',function(e){
@@ -83,30 +115,68 @@ $('.bestdiv div').on('click',function(e){
     e.preventDefault()
 })
 
-/* 풀어서 쓰기 */
-// $('#bestWrap section:eq(1), #bestWrap section:eq(2)').hide()
-// $('.bestdiv_best').on('click',function(e){
-//     $(this).parent().siblings('section').hide()
-//     $(this).parent().siblings('section:eq(0)').show()
-//     e.preventDefault()
-// })
-// $('.bestdiv_steady').on('click',function(e){
-//     $(this).parent().siblings('section').hide()
-//     $(this).parent().siblings('section:eq(1)').show()
-//     e.preventDefault()
-// })
-// $('.bestdiv_thisweek').on('click',function(e){
-//     $(this).parent().siblings('section').hide()
-//     $(this).parent().siblings('section:eq(2)').show()
-//     e.preventDefault()
-// })
-
-
 /* bestdiv클릭 시 class이동  */
+$('.bestdiv div').on('click',function(){
+    let posY=$(this).position().top
+    $('.divOn').stop().animate({top:posY},300)
+
+    $('.bestdiv div').children('a').css({color:'#000'})
+    $(this).children('a').css({color:'#fff'})
+})
 
 
+/* timeDeal */
+function CountDownTimer(dt, id) {
+    var end = new Date(dt)
+    var _second = 1000;
+    var _minute = _second * 60
+    var _hour = _minute * 60
+    var _day = _hour * 24
+    var timer;
+    function showRemaining() {
+        var now = new Date()
+        var distance = end - now
+        if (distance < 0) { //timeOut시
+            clearInterval(timer)
+            document.getElementById(id).innerHTML = '타임딜 종료됨'
+            return
+        }
+        var days = Math.floor(distance / _day)
+        var hours = Math.floor((distance % _day) / _hour)
+        var minutes = Math.floor((distance % _hour) / _minute)
+        var seconds = Math.floor((distance % _minute) / _second)
+        document.getElementById(id).innerHTML = days + '일 '
+        document.getElementById(id).innerHTML += hours + '시간 '
+        document.getElementById(id).innerHTML += minutes + '분 '
+        document.getElementById(id).innerHTML += seconds + '초'
+    }
+    timer = setInterval(showRemaining, 1000)
+}
+CountDownTimer('10/13/2021 23:59:59', 'dealCount') // 2021-10-13까지
 
+function alarm(){
+    $('#alarm').animate({top:"0"}).animate({left:"70px"},100).animate({left:"90px"},100).animate({left:"70px"},100).animate({left:"90px"},100).animate({top:"20px"})
+}
+setInterval(alarm,1000)
 
+/* farmWrap 슬라이드 */
+// $('#farmBtn div').on('click',function(){
+//     let pos=$(this).index()*(-800)
+//     $('#farmWrap').stop().animate({marginLeft:pos})
+
+//     $('#farmBtn div').removeClass('farmBtnOn')
+//     $(this).addClass('farmBtnOn')
+// })
+
+$('#farmBtn div').on('click',function(){
+    let num=$(this).index()
+    console.log( num )
+    $('#farmWrap li').removeClass('zIndex')
+    $('#farmWrap li:eq('+num+')').addClass('zIndex')
+
+    $('#farmBtn div').removeClass('farmBtnOn')
+    $(this).addClass('farmBtnOn')
+})
 
 
 
@@ -123,7 +193,6 @@ $(window).on('scroll',function(){
         $('#top').hide()
     }
 })
-// console.log( $('main').position().top )
 
 
 /*퀵버튼 토글 */
